a1aa586811c451b11088ccf772fd1f103b22380c
RnTs1002 <10223330403@stu.ecnu.edu.cn>
2023-09-20 08:26:47 +0800

    first commit

---
+++ Week1/1.7.py
@@ -0,0 +1,6 @@
+#输出1-100以内的奇数
+number = 1
+while number < 101:
+    if number % 2 == 1:
+        print(number)
+    number += 1
---
+++ Week1/1.9.while.py
@@ -0,0 +1,7 @@
+L1 = [1, 2, 3, 4, 5]
+L2 = []
+i = 4
+while i >= 0:
+    L2.append(L1[i])
+    i -= 1
+print(L2)
